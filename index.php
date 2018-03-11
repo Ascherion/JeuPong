563acc83314466c6ecb2bdf410ee7884bb811d0c
Ascherion <johan.desplats@gmail.com>
2018-03-11 20:25:33 +0100

    jojo save the world

--- index.php
+++ index.php
@@ -9,16 +9,16 @@
 </head>
 <body>
 	<div class="positiontitrepage1">
-		<h1><span class="nom">Kim </span><span class="titrepage1">PONG</span><span class="nom"> Un</span></h1>
+		<h1><span class="titrepage1">PONG</span></h1>
 	</div>
 
 	<div class="bouttonpage1">
 		<a href="public_html/pong.php"><button class="bouttonpage1-1"><img src="img/user.jpeg" class="imgpage1"> VS <img src="img/user.jpeg" class="imgpage1"></button></a>
 	</div>
-	
+
 	<div class="bouttonpage1">
 		<a href=""><button class="bouttonpage1-1"><img src="img/user.jpeg" class="imgpage1"> VS <img src="img/desktop.png" class="imgpage1"><div>"Coming soon ;)"</div></button></a>
 	</div>
-	
+
 </body>
 </html>
